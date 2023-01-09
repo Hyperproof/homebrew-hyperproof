a8777d4081776656df5ece8f3ab181b89898766f
Andrew Miller <andrew@westlakelabs.com>
2023-01-09 15:50:08 -0800

    Update binaries

--- Formula/hyperproof-cli.rb
+++ Formula/hyperproof-cli.rb
@@ -1,5 +1,5 @@
 class HyperproofCli < Formula
-  desc "Hyperproof CLI"
+  desc "Hyperproof Command-line Interface"
   homepage "https://hyperproof.io"
   url "https://downloads.hyperproof.app/hpcli/hpcli-binaries-0.8.7.tar.gz"
   sha256 "9e7b6ce9078d7e6d106a36421686996e4c6af66b02748fe0f7373aa92e1ab553"
@@ -13,6 +13,6 @@ class HyperproofCli < Formula
 
   test do
     version = shell_output("#{bin}/hp --version")
-    assert_match /0.8.7.[\d+]/, version
+    assert_match(/0.8.7.[\d+]/, version)
   end
 end
