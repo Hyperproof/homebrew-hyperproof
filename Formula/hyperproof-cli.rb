405ca122bf8a3faa4ff3add6dec6f33e2c9589d2
Andrew Miller <andrew@westlakelabs.com>
2024-11-18 07:15:03 -0800

    Bump HP CLI version

--- Formula/hyperproof-cli.rb
+++ Formula/hyperproof-cli.rb
@@ -1,7 +1,7 @@
 class HyperproofCli < Formula
   desc "Hyperproof Command-line Interface"
   homepage "https://hyperproof.io"
-  url "https://downloads.hyperproof.app/hpcli/hpcli-binaries-1.1.1.tar.gz"
+  url "https://downloads.hyperproof.app/hpcli/hpcli-binaries-1.2.2.tar.gz"
   sha256 "66bb1e0d1c91fd933cfc7ea80251fbe0a87c2ef8b5a6473f081c5b985c8cbcfd"
   license "MIT"
 
@@ -13,6 +13,6 @@ class HyperproofCli < Formula
 
   test do
     version = shell_output("#{bin}/hp --version")
-    assert_match(/1.1.1.[\d+]/, version)
+    assert_match(/1.2.2.[\d+]/, version)
   end
 end
